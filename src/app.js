c1225c1e4f732a2aa5fd381d3619a0023d0d988c
Siga Systems <sgswebdev@gmail.com>
2025-09-12 17:37:07 +0530

    Add initial HTML structure for Multi Tenant Application

--- src/app.js
+++ src/app.js
@@ -12,22 +12,35 @@ const app = express();
 
 // app.use(cors()); // Enable CORS for all routes
 
-app.use(cors({
-    origin: "*", // allow frontend
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}))
-
-
+app.use(
+    cors({
+        origin: "*", // allow frontend
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
 
 app.use(express.json()); // For JSON body parsing
 app.use(express.urlencoded({ extended: true })); // For URL-encoded body parsing
+
+// app.get("/", (req, res) => {
+//     res.send("Hello from the backend!");
+// });
 
 app.use("/tenant-requests", tenantRequestRoutes);
 app.use("/api/auth", authRouter); // Mount the auth routes
 app.use("/api/todos", todoRoutes); // Mount the todo routes
 
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("✅ Database connected...");
+    } catch (error) {
+        console.log(error);
+    }
+    console.log(`🚀 Server running on port ${PORT}`);
+});
 
 export default app;
